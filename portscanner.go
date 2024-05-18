1ae1be27e0b05e03b28b51873bcb97b8aeb24d57
jordan wallace <jordanwallace644@gmail.com>
2024-05-18 15:37:03 -0500

    refactor

--- portscanner.go
+++ portscanner.go
@@ -3,122 +3,31 @@ package main
 import (
 	"fmt"
 	"net"
-	"strconv"
 	"time"
 )
 
 func main() {
-	ch := make(chan int)
+	ch := make(chan int, 100)
 	var IP string
 	fmt.Printf("enter ip address here:")
 	fmt.Scan(&IP)
-
-	//        For most vunurable ports
-	//
-	// for port, vuln := range vulnPorts {
-	// 	go portScanner(IP, vuln, port, ch)
-	// 	<-ch
-	// }
-	//        Scan all existing ports
-	//
+	start := time.Now()
 	for port := 0; port < 65535; port++ {
-		go portScanner(IP, "No discription", port, ch)
+		ch <- port
+		go portScanner(IP, "No discription", port)
 		<-ch
-
 	}
+	close(ch)
+	fmt.Println(time.Now().Sub(start))
 }
-
-func portScanner(ip string, portDiscription string, port int, ch chan<- int) {
-	address := ip + ":" + strconv.Itoa(port)
-	_, err := net.DialTimeout("tcp", address, 10*time.Second)
+func portScanner(ip string, portDiscription string, port int) {
+	address := fmt.Sprintf("%s:%d", ip, port)
+	conn, err := net.DialTimeout("tcp", address, 5*time.Second)
 	if err == nil {
 		fmt.Printf("\n[+] port: %v\n-->  for : %v \n", port, portDiscription)
+	} else {
+		return
 	}
-	ch <- port
+	defer conn.Close()
 	return
-}
-
-var vulnPorts = map[int]string{
-	1:     "tcpmux",
-	7:     "echo",
-	9:     "discard",
-	11:    "systat",
-	13:    "daytime",
-	15:    "netstat",
-	17:    "qotd",
-	19:    "chargen",
-	20:    "ftp data",
-	21:    "ftp access",
-	22:    "ssh",
-	23:    "telnet",
-	25:    "smtp",
-	37:    "time",
-	42:    "name",
-	43:    "nicname",
-	53:    "domain",
-	69:    "tftp",
-	77:    "priv-rjs",
-	79:    "finger",
-	80:    "http",
-	87:    "ttylink",
-	95:    "supdup",
-	101:   "hostriame",
-	102:   "iso-tsap",
-	103:   "gppitnp",
-	104:   "acr-nema",
-	109:   "pop2",
-	110:   "pop3",
-	111:   "sunrpc",
-	113:   "auth",
-	115:   "sftp",
-	117:   "uucp-path",
-	119:   "nntp",
-	123:   "NTP",
-	135:   "loc-srv /epmap",
-	137:   "netbios",
-	139:   "netbios",
-	143:   "imap2",
-	161:   "snmp",
-	179:   "BGP",
-	389:   "ldap",
-	427:   "SLP (Also used by Apple Filing Protocol)",
-	465:   "smtp+ssl",
-	512:   "print / exec",
-	513:   "login",
-	514:   "shell",
-	515:   "printer",
-	526:   "tempo",
-	530:   "courier",
-	531:   "chat",
-	532:   "netnews",
-	540:   "uucp",
-	548:   "AFP (Apple Filing Protocol)",
-	554:   "rtsp",
-	556:   "remotefs",
-	563:   "nntp+ssl",
-	587:   "smtp (rfc6409)",
-	601:   "syslog-conn (rfc3195)",
-	636:   "ldap+ssl",
-	989:   "ftps-data",
-	990:   "ftps",
-	993:   "ldap+ssl",
-	995:   "pop3+ssl",
-	1719:  "h323gatestat",
-	1720:  "h323hostcall",
-	1723:  "pptp",
-	2049:  "nfs",
-	3000:  "Dev host",
-	3659:  "apple-sasl / PasswordServer",
-	4045:  "lockd",
-	5060:  "sip",
-	5061:  "sips",
-	6000:  "X11",
-	6566:  "sane-port",
-	6665:  "Alternate IRC [Apple addition]",
-	6666:  "Alternate IRC [Apple addition]",
-	6667:  "Standard IRC [Apple addition]",
-	6668:  "Alternate IRC [Apple addition]",
-	6669:  "Alternate IRC [Apple addition]",
-	6697:  "IRC +, TLS",
-	10080: " Amanda,",
 }
